73e76e0af0bc82e2a230d9fb99a47a933f566393
govi06s <dukkagovinda8179@gmail.com>
2024-08-29 02:26:58 +0530

    variab;es

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ram
 Person2=Krish
 
-echo "$Perason1 : : Hi {$Person2}, How are you?"
-echo "{$Person2} : : Hi $Person1, I am good what abiut you?"
+echo "$Person1 : : Hi {$Person2}, How are you?"
+echo "{$Person2} : : Hi $Person1, I am good what about you?"
 echo "$Person1 : : I'm good, waht are you doing $Person2?"
 echo "$Person2 : : I am learning Devops $Person1"
