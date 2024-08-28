e4cc63e1f27a094165080d8b94086ab9be6f03c7
govi06s <dukkagovinda8179@gmail.com>
2024-08-29 01:58:33 +0530

    added the conversation of two persons

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramki : : Hi Ramki, How are you?"
+echo "Govi : : Hi Govi, I am good what abiut you?"
+echo "Ramki : : I'm good, waht are you doing Govi?"
+echo "Govi : : I am learning Devops Ramki"
