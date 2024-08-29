b5c80854e7774f149dbf0c4b783474ac1d992fe1
govi06s <dukkagovinda8179@gmail.com>
2024-08-30 00:46:55 +0530

    passing the data outside through args

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,6 +5,6 @@ echo "Please enter the username: :"
 read -s USERNAME
 echo "Entered Username is: $USERNAME"
 
-echo "Please enter the username: :"
+echo "Please enter the password: :"
 read -s PASSWORD
 echo "Entered Password is: $PASSWORD"
