0700b15d6c1f5599ec8ee7da5ef53e9ac4fded83
govi06s <dukkagovinda8179@gmail.com>
2024-08-30 00:59:32 +0530

    passing the data outside through args

---
+++ 06-array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+FRUITS=("APPLE", "ORANGE", "BANANA")
+
+echo "First fruit is: ${Fruit[0]}"
+echo "Second fruit is: ${Fruit[1]}"
+echo "Third fruit is: ${Fruit[2]}"
+
+echo "All fruits are: ${Fruit[@]}"
