ca588169b862cca0e5aebc1f83bb00a57577e251
govi06s <dukkagovinda8179@gmail.com>
2024-08-31 13:49:55 +0530

    conditions

--- 12-functions.sh
+++ 12-functions.sh
@@ -7,10 +7,10 @@ USERID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "command is.. FAILED"
+        echo "$2 is.. FAILED"
         exit 1
     else
-        echo "command is.. SUCCESS"
+        echo "$2 is.. SUCCESS"
     fi
 }
 
