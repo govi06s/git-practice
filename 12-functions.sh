ac64ab58cdf1b796c3e50fc0679a129ba8bce01e
govi06s <dukkagovinda8179@gmail.com>
2024-08-31 13:44:22 +0530

    conditions

--- 12-functions.sh
+++ 12-functions.sh
@@ -17,35 +17,3 @@ fi
 dnf install git -y
 
 VALIDATE $?
-
-# if [ $? -ne 0 ]
-# then
-#     echo "git is not installed, going to install it.."
-#     dnf install git -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "git installation is not success... please check it"
-#         exit 1
-#     else
-#         echo "git installation sucsess... nothing do"
-#     fi
-# else
-#     echo "git is already installed, nothing to do anything.."
-# fi
-
-# dnf install mysql
-
-# if [ $? -ne 0 ]
-# then
-#     echo "MYSQL is not installed, going to install it.."
-#     dnf install mysql -y
-#     if [ $? -ne 0 ]
-#     then
-#         echo "MYSQL installation is not success.. please check once"
-#         exit 1
-#     else
-#         echo "MYSQL installation is success"
-#     fi
-# else
-#     echo "MYSQL installation was done, nothing to do"
-# fi
