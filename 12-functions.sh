8db15efd503995c80192f998c99c529b0d32e24b
govi06s <dukkagovinda8179@gmail.com>
2024-08-31 13:55:41 +0530

    conditions

--- 12-functions.sh
+++ 12-functions.sh
@@ -22,4 +22,22 @@ fi
 
 dnf list installed git
 
-VALIDATE $? "Listing MYSQL"
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to install it.."
+    dnf install git -y
+    VALIDATE $? "Installing git"
+else
+    echo "git is already installed, nothing to do anything.."
+fi
+
+dnf install mysql
+
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed, going to install it.."
+    dnf install mysql -y
+    VALIDATE $? "Installing MYSQL"
+else
+    echo "MYSQL installation was done, nothing to do"
+fi
