c84485e90a35f51504165956c046b161584f726c
govi06s <dukkagovinda8179@gmail.com>
2024-08-31 12:06:58 +0530

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 
 #echo "UserID is: $USERID"
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "please run the script with root preveleges"
-    exit 1
+
 fi
 
 dnf install git -y
