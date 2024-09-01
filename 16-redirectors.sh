ffae95ed36396bc9f46726fbc16d92ef96919090
govi06s <dukkagovinda8179@gmail.com>
2024-09-01 14:57:45 +0530

    loops

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -18,7 +18,7 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e " $R please rin the script with root prevelages $N" &>>$LOG_FILE
+        echo -e " $R please run the script with root prevelages $N" | tee -a &>>$LOG_FILE
         exit 1
     fi
 }
@@ -26,10 +26,10 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 is.. $R FAILED $N" &>>$LOG_FILE
+        echo "$2 is.. $R FAILED $N" | tee -a &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is.. $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "$2 is.. $G SUCCESS $N" |tee -a &>>$LOG_FILE
     fi
 }
 
@@ -37,7 +37,7 @@ USAGE(){
     echo -e "$R USAGE: : $N sudo sh 16-redirectors.sh package1 package2 ..."
     exit 1
 }
-echo "Script started excuting: $(date)" &>>$LOG_FILE
+echo "Script started excuting: $(date)" | tee -a &>>$LOG_FILE
 CHECK_ROOT
 if [ $# -eq 0 ]
 then
@@ -49,10 +49,10 @@ do
   dnf list installed $package &>>$LOG_FILE
   if [ $? -ne 0 ]
   then
-      echo "$package is not installed, going to install it.." &>>$LOG_FILE
+      echo "$package is not installed, going to install it.." | tee -a &>>$LOG_FILE
       dnf install $package -y &>>$LOG_FILE
       VALIDATE $? "Installing $package"
   else
-      echo -e "$package is already $Y installed, nothing to do anything.. $N" &>>$LOG_FILE
+      echo -e "$package is already $Y installed, nothing to do anything.. $N" |tee -a &>>$LOG_FILE
   fi
 done
